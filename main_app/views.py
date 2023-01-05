c5b8a4b9173c0c302fdec10b45b3dd7d53a6722f
randallka-ga <randallkaem@gmail.com>
2023-01-05 14:13:35 -0700

    base structure build

--- main_app/views.py
+++ main_app/views.py
@@ -2,14 +2,35 @@ from django.shortcuts import render, redirect
 from django.contrib.auth import login
 from django.contrib.auth.forms import UserCreationForm
 from .models import Favorite, Comment
+from django.contrib.auth.decorators import login_required
 
+import requests # allow us to make api calls
+headers = {
+	"X-RapidAPI-Key": "a249ee6bfamshfb62e9b1c9d89d2p17b949jsn9d35882504fe",
+	"X-RapidAPI-Host": "api-football-v1.p.rapidapi.com"
+}
 # Create your views here.
 
 def home(request):
-    return render(request, 'home.html')
+  url = "https://api-football-v1.p.rapidapi.com/v3/teams"
+  querystring = {"league": "39", "season": "2022"}
+  all_teams = requests.request("GET", url, headers=headers, params=querystring).json()
+  teams = all_teams['response']
+  favorite_ids = []
+  if request.user.is_authenticated: 
+    favorites = Favorite.objects.filter(user= request.user)
+    for favorite in favorites:
+      favorite_ids.append(favorite.team_id)
+  return render(request, 'home.html', {'teams' : teams, 'favorites' : favorite_ids})
 
-def team_detail(request):
-    return render(request, 'team.html')
+
+@login_required
+def team_detail(request, team_id):
+    url = "https://api-football-v1.p.rapidapi.com/v3/teams"
+    querystring = {"id": team_id}
+    team = requests.request(
+        "GET", url, headers=headers, params=querystring).json()
+    return render(request, 'team.html', {'team' : team['response'][0]})
 
 def game_detail(request):
     return render(request, 'game.html')
@@ -28,3 +49,16 @@ def signup(request):
   form = UserCreationForm()
   context = {'form': form, 'error_message': error_message}
   return render(request, 'registration/signup.html', context)
+
+@login_required
+def favorite_team(request, team_id): 
+  new_favorite = Favorite(team_id= team_id, user= request.user)
+  new_favorite.save()
+  return redirect('team_detail', team_id=team_id)
+
+
+@login_required
+def remove_team(request, team_id):
+  team = Favorite.objects.filter(team_id=team_id, user=request.user)
+  team.delete()
+  return redirect('/')
